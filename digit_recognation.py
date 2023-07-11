eddc599c9ffc1270c0ab44050c1a8ce8511982bb
prince-pansad <princepanwarsad@gmail.com>
2023-07-11 10:10:01 +0530

    digit recognition first commit2

--- digit_recognation.py
+++ digit_recognation.py
@@ -17,7 +17,7 @@ from tkinter import ttk
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 
-# digit recognition
+# digit recognition sytem
 
 DIGIT_WIDTH = 10
 DIGIT_HEIGHT = 20
